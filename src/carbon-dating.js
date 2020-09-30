c7465a4fbf327965e52c5a6a7b7a336739f2d435
Alexei Dudko <axo3@ya.ru>
2020-09-30 13:23:39 +0300

    feat: add count-cats and carbon-dating decisions code

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let sampleActivityNumber = parseInt(sampleActivity);
+  if (
+    isNaN(sampleActivityNumber) ||
+    typeof sampleActivity !== 'string' ||
+    sampleActivityNumber <= 0 ||
+    sampleActivityNumber > 15
+  ) return false;
+    
+  let rateConstant = Math.LN2 / HALF_LIFE_PERIOD;
+  let integratedRate = MODERN_ACTIVITY / parseInt(sampleActivity);
+  
+  return Math.ceil(Math.log2(integratedRate) / rateConstant);
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  // filter any of strings:  ^^, || ,^^, || ,^^ || ^^
+  const regExp = /(?<=(^|,))\^\^(?=(,|$))/g;
+  let catMatchesList = backyard.toString().match(regExp); // null or [results]
+  let catCount = catMatchesList ? catMatchesList.length : 0;
+  return catCount;
 };
